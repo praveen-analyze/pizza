7b3b2c2416bf2f2fadc13c1c897ecc9e8eed8177
praveen-analyze <epraveen952@example.com>
2026-05-31 10:29:32 +0530

    Update MongoDB connection and port configuration

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
+
 const pizzaRoutes = require("./routes/pizzaRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-const cors = require("cors");
 
 app.use(cors());
-
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/pizza")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
@@ -20,6 +20,8 @@ app.use("/api/pizzas", pizzaRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(3000, () => {
-  console.log("Server Started");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server Started on ${PORT}`);
 });
